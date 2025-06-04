2dc63fec9ec3d165073ce9b67ff9bb974ee813da
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-04 08:21:08 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Duba } from './Dupa.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Duba/>
+  </React.StrictMode>,
+);
